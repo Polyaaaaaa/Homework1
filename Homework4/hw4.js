ef39fb69b0dfbb968518ebda1593dc50812d1192
Polina <polinasatrajkina@gmail.com>
2026-06-10 22:23:48 +0300

    дофиналивание 4 домашки

--- Homework4/hw4.js
+++ Homework4/hw4.js
@@ -1,4 +1,43 @@
-let i = 1;
-while (i < 3) {
-    console.log('Привет')
+// Задание 1
+console.log('Привет');
+console.log('Привет');
+
+// Задание 2
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// Задание 3
+for (let i = 7; i <= 22; i++) {
+  console.log(i);
+}
+
+// Задание 4
+let obj = {
+  'Коля': '200',
+  'Вася': '300',
+  'Петя': '400'
+};
+
+for (let name in obj) {
+  console.log(name + ' — зарплата ' + obj[name] + ' долларов');
+}
+
+// Задание 5
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+  n = n / 2;
+  num++;
+}
+
+console.log(n);
+console.log(num);
+
+// Задание 6
+let firstFriday = 5;
+
+for (let day = firstFriday; day <= 31; day += 7) {
+  console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
 }
