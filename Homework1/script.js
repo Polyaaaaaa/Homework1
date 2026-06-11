a935fd19036f058cf72edf50d4b35727aad08edf
Polina <polinasatrajkina@gmail.com>
2026-06-11 22:46:38 +0300

    добавление квиза из домашки 6

--- Homework1/script.js
+++ Homework1/script.js
@@ -25,3 +25,37 @@ document.querySelectorAll('.games:not(.games_mobile) .card[data-game]').forEach(
         scrollToElement(document.querySelector(`#game-${card.dataset.game}`));
     });
 });
+
+// квиз
+
+const quiz = [
+  {
+    question: "Какой цвет небо?",
+    options: ["1. Красный", "2. Синий", "3. Зеленый"],
+    correctAnswer: 2
+  },
+  {
+    question: "Сколько дней в неделе?",
+    options: ["1. Шесть", "2. Семь", "3. Восемь"],
+    correctAnswer: 2
+  },
+  {
+    question: "Сколько у человека пальцев на одной руке?",
+    options: ["1. Четыре", "2. Пять", "3. Шесть"],
+    correctAnswer: 2
+  }
+];
+
+let correctAnswers = 0;
+
+for (let i = 0; i < quiz.length; i++) {
+  let userAnswer = Number(
+    prompt(quiz[i].question + "\n" + quiz[i].options.join("\n"))
+  );
+
+  if (userAnswer === quiz[i].correctAnswer) {
+    correctAnswers++;
+  }
+}
+
+alert(`Правильных ответов: ${correctAnswers}`);
